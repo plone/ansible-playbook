0f6038117ae9aefca740906eb58544c1a3a9bc12
Steve McMahon <steve@dcn.org>
2015-11-05 15:39:06 -0800

    Begin work on test framework

---
+++ tests.py
@@ -0,0 +1,14 @@
+import doctest
+import re
+import subprocess
+import sys
+
+options = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
+
+if '-f' not in sys.argv:
+    output = subprocess.check_output("vagrant status", shell=True)
+    if re.search(r"default\s+not created", output) is None:
+        print "Vagrant box already exists. Destroy it or use '-f' to skip this test."
+        exit(1)
+
+doctest.testfile('tests/sample-medium.txt', optionflags=options)
