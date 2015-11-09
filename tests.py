199568e48e96e0737cc1774577ea14c2f3ef1a5f
Steve McMahon <steve@dcn.org>
2015-11-09 10:00:00 -0800

    Testing improvements

--- tests.py
+++ tests.py
@@ -1,16 +1,22 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import doctest
+import glob
 import re
 import subprocess
 import sys
 
-options = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
+options = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
 
 if '-f' not in sys.argv:
     output = subprocess.check_output("vagrant status", shell=True)
     if re.search(r"default\s+not created", output) is None:
         print "Vagrant box already exists. Destroy it or use '-f' to skip this test."
+        print "Use '-f' in combination with '-np' to skip provisioning."
         exit(1)
 
-doctest.testfile('tests/sample-medium.txt', optionflags=options)
+for fn in glob.glob('tests/*.txt'):
+    print '*' * 50
+    print fn
+    print '*' * 50
+    doctest.testfile(fn, optionflags=options)
