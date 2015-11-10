b94a06b6d9dfcb361ce0ef8bf43f9a1afab22eb3
Steve McMahon <steve@dcn.org>
2015-11-10 08:19:56 -0800

    Test refinements

--- tests.py
+++ tests.py
@@ -8,6 +8,26 @@ import sys
 
 options = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
 
+# Convenience items for testing.
+
+devnull = open('/dev/null', 'w')
+
+
+def ssh_run(cmd):
+    return subprocess.check_output("""vagrant ssh -c '%s'""" % cmd, shell=True, stderr=devnull).replace('^@', '')
+
+
+def run(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    stdout, stderr = p.communicate()
+    if p.returncode:
+        print >> sys.stderr, stdout
+        # Stop the doctest
+        raise KeyboardInterrupt, stderr
+    return None
+
+globs = {'devnull': devnull, 'ssh_run': ssh_run, 'run': run}
+
 if '-f' not in sys.argv:
     output = subprocess.check_output("vagrant status", shell=True)
     if re.search(r"default\s+not created", output) is None:
@@ -19,4 +39,4 @@ for fn in glob.glob('tests/*.txt'):
     print '*' * 50
     print fn
     print '*' * 50
-    doctest.testfile(fn, optionflags=options)
+    doctest.testfile(fn, optionflags=options, globs=globs)
