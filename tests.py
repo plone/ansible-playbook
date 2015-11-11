9e981df511cc450fdca410a21f091bff46de0e44
Steve McMahon <steve@dcn.org>
2015-11-11 13:39:15 -0800

    Refactor for maintainability

--- tests.py
+++ tests.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 
+import argparse
 import doctest
 import glob
 import re
 import subprocess
 import sys
+
+parser = argparse.ArgumentParser(description='Run playbook tests.')
+parser.add_argument(
+    '-f', '--force',
+    action='store_true',
+    help="Force tests to proceed if box already exists."
+    )
+parser.add_argument(
+    '-np',
+    '--no-provision',
+    action='store_true',
+    help="Skip provisioning."
+    )
+parser.add_argument(
+    '--file',
+    help="Specify a single doctest file.",
+    )
+
+args = parser.parse_args()
 
 options = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
 
@@ -26,17 +46,30 @@ def run(cmd):
         raise KeyboardInterrupt, stderr
     return None
 
-globs = {'devnull': devnull, 'ssh_run': ssh_run, 'run': run}
+globs = {
+    'devnull': devnull,
+    'ssh_run': ssh_run,
+    'run': run,
+    'skip_provisioning': args.no_provision,
+    'forcing': args.force
+    }
 
-if '-f' not in sys.argv:
+if not args.force:
     output = subprocess.check_output("vagrant status", shell=True)
     if re.search(r"default\s+not created", output) is None:
         print "Vagrant box already exists. Destroy it or use '-f' to skip this test."
         print "Use '-f' in combination with '-np' to skip provisioning."
         exit(1)
 
-for fn in glob.glob('tests/*.txt'):
+if args.file is None:
+    files = glob.glob('tests/*.txt')
+else:
+    files = [args.file]
+
+for fn in files:
     print '*' * 50
     print fn
     print '*' * 50
-    doctest.testfile(fn, optionflags=options, globs=globs)
+    failure_count, test_count = doctest.testfile(fn, optionflags=options, globs=globs)
+    if failure_count > 0:
+        exit(1)
